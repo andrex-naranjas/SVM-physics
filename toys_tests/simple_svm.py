eb4b4d6c4ee9e606c329a62a0f6901cfe488726c
Andres Ramirez Morales <andrex_naranjas@flavourmac.local>
2023-11-30 20:58:40 -0600

    add cylindrical

--- toys_tests/simple_svm.py
+++ toys_tests/simple_svm.py
@@ -1,10 +1,15 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC
+from sklearn.neural_network import MLPClassifier
 from sklearn.metrics import accuracy_score
 
 # Step 1: Load the data from the CSV file
-data = pd.read_csv("spherical_data.csv")
+data_sph = pd.read_csv("spherical_data.csv")
+data_cyl = pd.read_csv("spherical_data.csv")
+data = pd.read_csv("cylindrical_spherical_data.csv")
+#data = pd.concat([data_sph, data_cyl])
+#print(data.shape, data_cyl.shape)
 
 # Assuming the target column is named 'class', and you have feature columns named 'feature1', 'feature2', etc.
 X = data.drop('class', axis=1)
@@ -14,10 +19,11 @@ y = data['class']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # Step 3: Create and train the SVM classifier
-svm_classifier = SVC(kernel='poly', degree=2)  # You can choose different kernels such as 'linear', 'rbf', 'poly', etc.
-#svm_classifier = SVC(kernel='linear')  # You can choose different kernels such as 'linear', 'rbf', 'poly', etc.
+svm_classifier = SVC(kernel='rbf')  # You can choose different kernels such as 'linear', 'rbf', 'poly', etc.
+svm_classifier = SVC(kernel='linear')  # You can choose different kernels such as 'linear', 'rbf', 'poly', etc.
 svm_classifier.fit(X_train, y_train) # fit -> teach
 
+# a partir de aqui, en principio a 
 # Step 4: Make predictions on the test set
 y_pred = svm_classifier.predict(X_test)
 print(y_pred)
--- toys_tests/toy_spherical.py
+++ toys_tests/toy_spherical.py
@@ -28,6 +28,33 @@ def generate_spherical_data(num_points, radius=1.0, data_class=1):
     spherical_data_class = np.column_stack((x, y, z, class_vec))
     return spherical_data, spherical_data_class
 
+
+def generate_cylindrical_data(num_points, radius=1.0, data_class=1):
+    """
+    Generate random points on the surface of a cylinder.
+
+    Parameters:
+    - num_points: The number of points to generate.
+    - radius: The radius of the cylinder.
+    - data_class: class of objects
+
+    Returns:
+    - An array of shape (num_points, 3) containing (x, y, z) coordinates of the points on the sphere.
+    """
+    radius = np.random.uniform(radius-1, radius, num_points)  # Random radius values
+    theta = np.random.uniform(0, 2*np.pi, num_points)  # Random angle values
+    height = np.random.uniform(1, 10, num_points)  # Random height values
+    class_vec = np.full(num_points, data_class)
+
+    x = radius * np.cos(theta)
+    y = radius * np.sin(theta)
+    z = height
+
+    cylindrical_data = np.column_stack((x, y, z))
+    cylindrical_data_class = np.column_stack((x, y, z, class_vec))
+    return cylindrical_data, cylindrical_data_class
+
+
 # Generate spherical data
 num_points = 1000
 radius1 = 5.0
@@ -40,8 +67,7 @@ print(spherical_data_1_class)
 df_sph1 = pd.DataFrame(spherical_data_1_class, columns = ['X','Y','Z', 'class'])
 df_sph2 = pd.DataFrame(spherical_data_2_class, columns = ['X','Y','Z', 'class'])
 df_tot = pd.concat([df_sph1, df_sph2], axis=0)
-print(df_tot)
-
+#print(df_tot)
 df_tot.to_csv('spherical_data.csv', index=False)
 
 # Plot spherical data
@@ -54,5 +80,54 @@ ax.set_xlabel('X')
 ax.set_ylabel('Y')
 ax.set_zlabel('Z')
 ax.set_title('Spherical Data Plot')
-
 plt.show()
+plt.clf()
+
+# genenerate cylindrical data
+num_points = 1000
+radius1 = 5.0
+radius2 = 10.0
+cylindrical_data_1, cylindrical_data_1_class = generate_cylindrical_data(num_points, radius1, 1)
+cylindrical_data_2, cylindrical_data_2_class = generate_cylindrical_data(num_points, radius2, -1)
+
+df_cyl1 = pd.DataFrame(cylindrical_data_1_class, columns = ['X','Y','Z', 'class'])
+df_cyl2 = pd.DataFrame(cylindrical_data_2_class, columns = ['X','Y','Z', 'class'])
+df_tot_c = pd.concat([df_cyl1, df_cyl2], axis=0)
+df_tot.to_csv('cylindrical_data.csv', index=False)
+
+# store sum
+df_sum_sc_1 = pd.DataFrame(cylindrical_data_1_class+spherical_data_1_class, columns = ['X','Y','Z', 'class'])
+df_sum_sc_2 = pd.DataFrame(cylindrical_data_2_class+spherical_data_2_class, columns = ['X','Y','Z', 'class'])
+df_tot_sum_c = pd.concat([df_cyl1, df_cyl2], axis=0)
+df_tot.to_csv('cylindrical_spherical_data.csv', index=False)
+
+# Plot cylindrical data
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.scatter(cylindrical_data_1[:, 0], cylindrical_data_1[:, 1], cylindrical_data_1[:, 2], c='b', marker='o')
+ax.scatter(cylindrical_data_2[:, 0], cylindrical_data_2[:, 1], cylindrical_data_2[:, 2], c='r', marker='x')
+
+ax.set_xlabel('X')
+ax.set_ylabel('Y')
+ax.set_zlabel('Z')
+ax.set_title('Cylindrical Data Plot')
+plt.show()
+plt.clf()
+
+
+# Plot sum of data
+#df_add = df1.add(df2, fill_value=0)
+#df_cyl1
+#df_cyl2
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.scatter((cylindrical_data_1+spherical_data_1)[:, 0], (cylindrical_data_1+spherical_data_1)[:, 1], (cylindrical_data_1+spherical_data_1)[:, 2], c='b', marker='o')
+ax.scatter((cylindrical_data_2+spherical_data_2)[:, 0], (cylindrical_data_2+spherical_data_2)[:, 1], (cylindrical_data_2+spherical_data_2)[:, 2], c='r', marker='x')
+
+ax.set_xlabel('X')
+ax.set_ylabel('Y')
+ax.set_zlabel('Z')
+ax.set_title('Cylindrical Data Plot')
+plt.show()
+plt.clf()
